cc6b27dad3ffcf06eb13ace0a2c2d0f3897792e4
Matheuskocotem <Kocotemmatheus@gmail.com>
2024-10-19 23:33:39 -0300

    const

---
+++ web/src/consts.js
@@ -0,0 +1,4 @@
+export default {
+    salaoId: '6708957fd2fb8d2b7ed34188',
+    clienteId: '67145f4d4cb156447e20e93d',
+}
--- web/src/routes.js
+++ web/src/routes.js
@@ -1,24 +1,32 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './styles.css'
+import './styles.css';
 
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
-const Routes = () => {
-    return(
-        <>
-            <Header/>
-            <div className="container-fluid h-100">
-                <div className="row h-100">
-                    <Router>
-                        <Sidebar/>
-                    </Router>
-                </div>
-            
-            </div>
-        </>
-    );
+import Agendamentos from "./pages/Agendamentos";
+import Clientes from "./pages/Clientes";
+
+const AppRoutes = () => {
+  return (
+    <>
+      <Header />
+      <div className="container-fluid h-100">
+        <div className="row h-100">
+          <Router>
+            <Sidebar />
+            <main className="col">
+              <Routes>
+                <Route path="/" element={<Agendamentos />} />
+                <Route path="/clientes" element={<Clientes />} />
+              </Routes>
+            </main>
+          </Router>
+        </div>
+      </div>
+    </>
+  );
 };
 
-export default Routes;
+export default AppRoutes;
