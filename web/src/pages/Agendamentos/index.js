527d01727a1f1bf1965ee2413213c0d2e5a12a5a
Matheuskocotem <Kocotemmatheus@gmail.com>
2024-10-20 14:17:24 -0300

    debugando erro

--- web/src/pages/Agendamentos/index.js
+++ web/src/pages/Agendamentos/index.js
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -8,39 +7,39 @@ import { useDispatch } from 'react-redux';
 const localizer = momentLocalizer(moment);
 
 const Agendamentos = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch({
+      type: '@agendamento/FILTER',
+      start: moment().weekday(0).format('YYYY-MM-DD'),
+      end: moment().weekday(6).format('YYYY-MM-DD'),
+    });
+  }, [dispatch]); // Corrigido para usar a dependência de dispatch
 
-    useEffect(() => {
-        useDispatch({
-            type: '@agendamento/FILTER',
-            start: moment().weekday(0).format('YYYY-MM-DD'),
-            end: moment().weekday(6).format('YYYY-MM-DD'),
-        })
-    }), []);
-
-
-    return (
-        <div className="col p-5 overflow-auto h-100">
-           <div className="row">
-              <div className="col-12">
-                <h2 className="mb-4 mt-0">Agendamentos</h2>
-                <Calendar
-                    localizer={localizer}
-                    events={[
-                        { title: "Evento Teste", 
-                            start: moment().toDate(), 
-                            end: moment().add(90, 'minutes').toDate()},
-                    ]}
-                    defaultView="week"
-                    selectable
-                    popup
-                    style={{ height: 700 }} 
-                />
-              </div>
-           </div> 
+  return (
+    <div className="col p-5 overflow-auto h-100">
+      <div className="row">
+        <div className="col-12">
+          <h2 className="mb-4 mt-0">Agendamentos</h2>
+          <Calendar
+            localizer={localizer}
+            events={[
+              {
+                title: 'Evento Teste',
+                start: moment().toDate(),
+                end: moment().add(90, 'minutes').toDate(),
+              },
+            ]}
+            defaultView="week"
+            selectable
+            popup
+            style={{ height: 700 }}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Agendamentos;
--- web/src/store/modules/agendamento/reducer.js
+++ web/src/store/modules/agendamento/reducer.js
@@ -2,15 +2,15 @@ const INITIAL_STATE = {
     agendamentos: [],
 };
 
-
 function agendamento(state = INITIAL_STATE, action) {
     switch (action.type) {
-        case '@agendamento/ALL': {
-            //
-        }
+        case '@agendamento/ALL':
+            // eslint-disable-next-line no-fallthrough
+            return state;
+
         default: 
             return state;
-    } 
+    }
 }
 
 export default agendamento;
