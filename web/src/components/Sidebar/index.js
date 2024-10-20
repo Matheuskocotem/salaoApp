3c5aeb1970d4a2f772b9915979f7b958332b3150
Matheuskocotem <Kocotemmatheus@gmail.com>
2024-10-19 23:34:04 -0300

    sidebar

--- web/src/components/Sidebar/index.js
+++ web/src/components/Sidebar/index.js
@@ -1,5 +1,26 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png'; 
+
 const Sidebar = () => {
-    return <h1>Sidebar</h1>
+  return (
+    <aside className="col-2 h-100">
+      <img src={logo} alt="Logo" className="img-fluid px-3 py-4" />
+      <ul className="p-0 m-0">
+        <li>
+          <Link to="/">
+            <span className="mdi mdi-calendar-check"></span>
+            <span>Agendamentos</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/clientes">
+            <span className="mdi mdi-account-multiple"></span>
+            <span>Clientes</span>
+          </Link>
+        </li>
+      </ul>
+    </aside>
+  );
 };
 
 export default Sidebar;
