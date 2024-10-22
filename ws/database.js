a5b1e93d9c1fe9f5aaa74f1c2f25be6f3b168b75
Matheuskocotem <kocotemmatheus@gmail.com>
2024-10-22 15:50:45 -0300

    banco

--- ws/database.js
+++ ws/database.js
@@ -1,15 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI = '';  // mongodb+srv://salaoUser:brIBzx32MiodJOD3@cluster0.lky72.mongodb.net/salaoUser?retryWrites=true&w=majority&appName=Cluster0&connectTimeoutMS=30000&socketTimeoutMS=60000
+const URI = ''; //mongodb+srv://salaoUser:brIBzx32MiodJOD3@cluster0.lky72.mongodb.net/salaoUser?retryWrites=true&w=majority&appName=Cluster0&connectTimeoutMS=30000&socketTimeoutMS=60000
 
-const env = process.env.NODE_ENV || 'dev';
-let options = {
-  useUnifiedTopology: true, 
-  useNewUrlParser: true,
-
-};
-
-mongoose
-  .connect(URI, options)
+mongoose.connect(URI)
   .then(() => console.log('DB is Up!'))
   .catch((err) => console.log(err));
